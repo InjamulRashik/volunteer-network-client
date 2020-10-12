3614db009cf9db08159ee7511ddcd59f35695e2e
Injamul Haque Rashik <ihrashik@gmail.com>
2020-10-12 17:07:43 +0600

    Router Added!

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import "./App.css";
 import Home from "./components/Home/Home";
 import Login from "./components/Login/Login";
 import Registration from "./components/Registration/Registration";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Home></Home>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/registration">
+          <Registration />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
